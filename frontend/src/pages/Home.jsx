6043a42958176fb9311bafb4dcc6abde70cc9e1a
Sadik-Sami <miasimanto@gmail.com>
2025-02-11 23:48:16 +0600

    added product details component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
 import NewArrivals from '../components/Products/NewArrivals';
+import ProductDetails from '../components/Products/ProductDetails';
 
 const Home = () => {
 	return (
@@ -10,6 +11,7 @@ const Home = () => {
 			<NewArrivals />
 			{/* Best Seller */}
 			<h2 className='text-3xl text-center font-bold mb-4'>Best Seller</h2>
+			<ProductDetails />
 		</div>
 	);
 };
