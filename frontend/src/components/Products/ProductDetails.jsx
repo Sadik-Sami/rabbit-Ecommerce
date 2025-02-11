fe51dc62cdf847dc3cbf6785d572536c088a3f5d
Sadik-Sami <miasimanto@gmail.com>
2025-02-12 00:00:54 +0600

    added add to cart functionality with simulation

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 const selectedProduct = {
 	name: 'Stylish Jacket',
@@ -37,6 +38,21 @@ const ProductDetails = () => {
 		if (action === 'plus') setQuantity((prev) => prev + 1);
 		if (action === 'minus' && quantity > 1) setQuantity((prev) => prev - 1);
 	};
+	const handleAddToCart = () => {
+		if (!selectedSize || !selectedColor) {
+			toast.error('Please select a size and color before adding to cart.', {
+				duration: 1000,
+			});
+			return;
+		}
+
+		setIsButtonDisabled(true);
+		setTimeout(() => {
+			toast.success('Product added to cart!', { duration: 1000 });
+			setIsButtonDisabled(false);
+		}, 500);
+	};
+
 	return (
 		<div className='p-6'>
 			<div className='max-w-6xl mx-auto bg-white p-8 rounded-lg'>
@@ -126,7 +142,14 @@ const ProductDetails = () => {
 							</div>
 						</div>
 						{/* Add to cart */}
-						<button className='bg-black text-white py-2 px-6 rounded w-full mb-4'>ADD TO CART</button>
+						<button
+							onClick={handleAddToCart}
+							className={`bg-black text-white py-2 px-6 rounded w-full mb-4 ${
+								isButtonDisabled ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-900'
+							}`}
+							disabled={isButtonDisabled}>
+							{isButtonDisabled ? 'Adding...' : 'ADD TO CART'}
+						</button>
 						<div className='mt-10 text-gray-700'>
 							<h3 className='text-xl font-bold mb-4'>Characteristics:</h3>
 							<table className='w-full text-left text-sm text-gray-600'>
