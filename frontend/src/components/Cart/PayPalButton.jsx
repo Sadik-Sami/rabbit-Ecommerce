2048fafd2ea3e1414472b5ef6fb919c9fcdeeb2c
Sadik-Sami <miasimanto@gmail.com>
2025-02-16 18:52:02 +0600

    added paypal payment

--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -1,6 +1,23 @@
-const PayPalButton = () => {
-  return (
-    <div>PayPalButton</div>
-  )
-}
-export default PayPalButton
+import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
+const PayPalButton = ({ amount, onSuccess, onError }) => {
+	return (
+		<PayPalScriptProvider
+			options={{
+				clientId: 'AaEZIgHhLI5OwqNu9DEwp688siDs2T1-ONRmdbwr9a-gwU6gV0mJ-02TZ6YIeDJYgt_UbgW0IUakJBi9',
+			}}>
+			<PayPalButtons
+				style={{ layout: 'vertical' }}
+				createOrder={(data, actions) => {
+					return actions.order.create({
+						purchase_units: [{ amount: { value: amount } }],
+					});
+				}}
+				onApprove={(data, actions) => {
+					return actions.order.capture().then(onSuccess);
+				}}
+				onError={onError}
+			/>
+		</PayPalScriptProvider>
+	);
+};
+export default PayPalButton;
