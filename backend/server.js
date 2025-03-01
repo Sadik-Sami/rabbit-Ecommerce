256559da2d06ded8d43105b160500e8ebd4a6433
Sadik-Sami <miasimanto@gmail.com>
2025-03-02 01:34:18 +0600

    created backend base

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 9000;
+
+app.get('/', (req, res) => {
+	res.send('Welcome to Rabbit API');
+});
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
