c67fdc2e2e475e3f07a4852f8a0c0df4e45adbcb
Sadik-Sami <miasimanto@gmail.com>
2025-03-04 00:48:59 +0600

    created login route and tested

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,10 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
-// @route Post /api/users/register
+
+// @route POST /api/users/register
 // @desc register a new user
 // @access Public
-
 router.post('/register', async (req, res) => {
 	const { name, email, password } = req.body;
 	try {
@@ -36,6 +36,42 @@ router.post('/register', async (req, res) => {
 	} catch (error) {
 		console.log(error);
 		res.status(500).send('Server Error');
+	}
+});
+
+// @route POST /api/users/register
+// @desc Authenticate user
+// @access Public
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		// Find the user by email
+		let user = await User.findOne({ email });
+		if (!user) return res.status(400).json({ message: 'Invalid Credentials' });
+
+		const isMatch = await user.matchPassword(password);
+		if (!isMatch) return res.status(400).json({ message: 'Invalid Credentials' });
+
+		// Create JWT Payload
+		const payload = { user: { id: user._id, role: user.role } };
+		// Sign and return the token along with user data
+		jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '6h' }, (err, token) => {
+			if (err) throw err;
+
+			// send the user and token in response
+			res.json({
+				user: {
+					_id: user._id,
+					name: user.name,
+					email: user.email,
+					role: user.role,
+				},
+				token,
+			});
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Server Error' });
 	}
 });
 
