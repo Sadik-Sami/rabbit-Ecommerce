b73f607af6014fbb3cc406a9b4f0279242126ce8
Sadik Sami <miasimanto@gmail.com>
2025-07-29 20:21:53 +0600

    added backend routes for creating cart

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Helper function to get cart by userId or guestId
+const getCart = async (userId, guestId) => {
+	if (userId) {
+		return await Cart.findOne({ userId: userId });
+	} else if (guestId) {
+		return await Cart.findOne({ guestId: guestId });
+	} else return null;
+};
+
+// @route POST /api/cart
+// @desc Add a product to the cart for a guest or logged-in user
+// @access Public
+router.post('/', protect, async (req, res) => {
+	const { productId, quantity, size, color, guestId, userId } = req.body;
+	try {
+		const product = await Product.findById(productId);
+		if (!product) {
+			return res.status(404).json({ message: 'Product not found' });
+		}
+		// Determine if the request is from a guest or a logged-in user
+		let cart = await getCart(userId, guestId);
+
+		// if the cart exists, update it; otherwise, create a new one
+		if (cart) {
+			const productIndex = cart.products.findIndex(
+				(p) => p.productId.toString() === productId && p.size === size && p.color === color
+			);
+			if (productIndex > -1) {
+				// if the product already exists in the cart, update the quantity
+				cart.products[productIndex].quantity += quantity;
+			} else {
+				// add new product to the cart
+				cart.products.push({
+					productId,
+					name: product.name,
+					image: product.images[0].url,
+					price: product.price,
+					size,
+					color,
+					quantity,
+				});
+			}
+			// Recalculate the total price
+			cart.totalPrice = cart.products.reduce((acc, item) => acc + item.price * item.quantity, 0);
+			await cart.save();
+			return res.status(200).json(cart);
+		} else {
+			// Create a new cart for the guest or user
+			const newCart = await Cart.create({
+				userId: userId ? userId : undefined,
+				guestId: guestId ? guestId : 'guest_' + new Date().getTime(),
+				products: [
+					{
+						productId,
+						name: product.name,
+						image: product.images[0].url,
+						price: product.price,
+						size,
+						color,
+						quantity,
+					},
+				],
+				totalPrice: product.price * quantity,
+			});
+			return res.status(201).json(newCart);
+		}
+	} catch (error) {
+		console.error('Error adding product to cart:', error);
+		return res.status(500).json({ message: 'Server error', error: error.message });
+	}
+});
+module.exports = router;
