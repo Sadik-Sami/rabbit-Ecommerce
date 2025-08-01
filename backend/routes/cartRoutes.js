eeb7395d957ded3d4b8ca95f2f5ecaecc89d1fae
Sadik Sami <miasimanto@gmail.com>
2025-08-01 19:15:01 +0600

    added product removing from cart route

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -109,4 +109,31 @@ router.put('/', async (req, res) => {
 	}
 });
 
+// @route Delete /api/cart
+// @desc remove a product from the cart
+// access Public
+router.delete('/', async (req, res) => {
+	const { productId, size, color, guestId, userId } = req.body;
+	try {
+		let cart = await getCart(userId, guestId);
+		if (!cart) return res.status(404).json({ message: 'Cart not found' });
+		const productIndex = cart.products.findIndex(
+			(p) => p.productId.toString() === productId && p.size === size && p.color === color
+		);
+		if (productIndex > -1) {
+			// Remove product from cart
+			cart.products.splice(productIndex, 1);
+			// Recalculate total price
+			cart.totalPrice = cart.products.reduce((acc, item) => acc + item.price * item.quantity, 0);
+			await cart.save();
+			return res.status(200).json(cart);
+		} else {
+			return res.status(404).json({ message: 'Product not found in cart' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Server Error' });
+	}
+});
+
 module.exports = router;
