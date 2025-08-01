36920790657bf0225d2aacc81d9b6e963ebbde1d
Sadik Sami <miasimanto@gmail.com>
2025-08-01 15:48:26 +0600

    added product quantity update route

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -17,7 +17,7 @@ const getCart = async (userId, guestId) => {
 // @route POST /api/cart
 // @desc Add a product to the cart for a guest or logged-in user
 // @access Public
-router.post('/', protect, async (req, res) => {
+router.post('/', async (req, res) => {
 	const { productId, quantity, size, color, guestId, userId } = req.body;
 	try {
 		const product = await Product.findById(productId);
@@ -76,4 +76,37 @@ router.post('/', protect, async (req, res) => {
 		return res.status(500).json({ message: 'Server error', error: error.message });
 	}
 });
+
+// @route PUT /api/cart
+// @desc Update a product quantity in the cart for a guest or logged-in user
+// @access Public
+router.put('/', async (req, res) => {
+	const { productId, quantity, size, color, guestId, userId } = req.body;
+	try {
+		let cart = await getCart(userId, guestId);
+		if (!cart) return res.status(404).json({ message: 'Cart not found' });
+
+		const productIndex = cart.products.findIndex(
+			(p) => p.productId.toString() === productId && p.size === size && p.color === color
+		);
+		if (productIndex > -1) {
+			// update quantitiy
+			if (quantity > 0) {
+				cart.products[productIndex].quantity = quantity;
+			} else {
+				// Remove product if quantity is 0 [splice(start, deleteCount)]
+				cart.products.splice(productIndex, 1);
+			}
+			cart.totalPrice = cart.products.reduce((acc, item) => acc + item.price * item.quantity, 0);
+			await cart.save();
+			return res.status(200).json(cart);
+		} else {
+			return res.status(404).json({ message: 'Product not found in cart' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Server Error' });
+	}
+});
+
 module.exports = router;
